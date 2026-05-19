23815553d9727d1daa96f545344bcd30305ae833
混的人 <2223544759@qq.com>
2026-05-19 13:26:22 +0800

    check in

---
+++ oop_basis/extend.java
@@ -0,0 +1,43 @@
+public class Main {
+    public static void main(String[] args) {
+        Person p = new Person("小明", 12);
+        Student s = new Student("小红", 20, 99);
+        Student ps = new PrimaryStudent("小军", 9, 100, 5);
+        System.out.println(ps.getScore());
+    }
+}
+
+class Person {
+    protected String name;
+    protected int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public int getAge() { return age; }
+    public void setAge(int age) { this.age = age; }
+}
+
+class Student extends Person {
+    protected int score;
+
+    public Student(String name, int age, int score) {
+        super(name, age);
+        this.score = score;
+    }
+
+    public int getScore() { return score; }
+}
+
+class PrimaryStudent extends Student{
+    protected double grade;
+    PrimaryStudent(String name, int age, int score, double grade){
+        super(name, age, score);
+        this.grade = grade;
+    }
+}
