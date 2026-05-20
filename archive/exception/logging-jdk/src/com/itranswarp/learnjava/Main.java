b6ab88b7c5fb56eace23e948087c0492a762faae
混的人 <2223544759@qq.com>
2026-05-20 22:15:40 +0800

    check in

---
+++ archive/exception/logging-jdk/src/com/itranswarp/learnjava/Main.java
@@ -0,0 +1,24 @@
+package com.itranswarp.learnjava;
+
+import java.io.UnsupportedEncodingException;
+import java.util.logging.Logger;
+
+/**
+ * Learn Java from https://www.liaoxuefeng.com/
+ * 
+ * @author liaoxuefeng
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		Logger logger = Logger.getLogger(Main.class.getName());
+		logger.info("Start process...");
+		try {
+			"".getBytes("invalidCharsetName");
+		} catch (UnsupportedEncodingException e) {
+			logger.severe("unsupported encoding");
+		}
+
+		logger.info("Process end.");
+	}
+}
